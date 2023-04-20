b13e90be76a98e1ea46d25768e947becede07308
Jin Han <120617717+Esther2112@users.noreply.github.com>
2023-04-20 10:52:21 +0900

    Test Class

--- src/main/java/com/spring/mvc/chap04/repository/ScoreRepositoryImpl.java
+++ src/main/java/com/spring/mvc/chap04/repository/ScoreRepositoryImpl.java
@@ -1,27 +1,55 @@
 package com.spring.mvc.chap04.repository;
 
+import com.spring.mvc.chap04.entity.Grade;
 import com.spring.mvc.chap04.entity.Score;
 
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class ScoreRepositoryImpl implements ScoreRepository {
+
+    //key : 학번, value : 성적정보
+    private static final Map<Integer, Score> scoreMap;
+
+    //학번에 사용할 일련번호
+    private static int sequence;
+     static {
+         scoreMap = new HashMap<>();
+         Score stu1 = new Score("뽀로로", 100, 50, 20, ++sequence, 0, 0, Grade.A);
+         Score stu2 = new Score("뿌루루", 30, 55, 90, ++sequence,  0, 0, Grade.A);
+         Score stu3 = new Score("뚜뚜루", 40, 80, 100, ++sequence,  0, 0, Grade.A);
+
+         scoreMap.put(stu1.getStuNum(), stu1);
+         scoreMap.put(stu2.getStuNum(), stu2);
+         scoreMap.put(stu3.getStuNum(), stu3);
+     }
     @Override
     public List<Score> findAll() {
-        return null;
+         return new ArrayList<>(scoreMap.values())
+                 .stream()
+                 .sorted(Comparator.comparing(Score::getStuNum))
+                 .collect(Collectors.toList());
     }
 
     @Override
     public boolean save(Score score) {
-        return false;
+         if(scoreMap.containsKey(score.getStuNum())) {
+             return false;
+         }
+         score.setStuNum(++sequence);
+         scoreMap.put(score.getStuNum(), score);
+        return true;
     }
 
     @Override
     public boolean deleteByStuNum(int stuNum) {
-        return false;
+         if(!scoreMap.containsKey(stuNum)) return false;
+         scoreMap.remove(stuNum);
+         return true;
     }
 
     @Override
     public Score findByStuNum(int stuNum) {
-        return null;
+        return scoreMap.get(stuNum);
     }
 }
